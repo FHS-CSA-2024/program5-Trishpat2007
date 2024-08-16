32c6076407e2a9dba6dbc68c6965a1c718829aae
Trishpat2007 <trishpat2007@gmail.com>
2024-08-16 09:27:52 -0700

    Update Program5.java
    update need to finish rounding

--- Program5.java
+++ Program5.java
@@ -1,7 +1,23 @@
 //import stuff here?
 
 //Your code here
-
+class Program5{
+    public static void main(String[] args){
+        System.out.println("Mushroom Cup Prix Racer Average Miles/Per Gallon");
+        int royaleMiles= 286;
+        int koopaMiles= 412;
+        int pipeMiles= 361;
+        int bandwagonMiles= 161;
+        
+        double royaleAvrg= (royaleMiles/9.0)*100;
+        royaleAvrg= (int)royaleAvrg;
+        double koopaAvrg= koopaMiles/40;
+        double pipeAvrg= pipeMiles/18;
+        double bandwagonAvrg= bandwagonMiles/11;
+        System.out.println("Royale averaged "+royaleAvrg);
+        
+    }
+}
 //Paste console output below:
 /*
 
